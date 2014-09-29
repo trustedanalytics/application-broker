c62cffef5f2a65c3fe93ba26071c377128ce3d69
mchmarny <mark@chmarny.com>
2014-09-29 11:00:54 -0700

    One more name to change

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./generic-cf-service-broker -h=127.0.0.1 \
-                            -p=9999 \
-                            -u=user \
-                            -s=pswd \
-                            -d=true \
-                            -src=appname \
-                            -url=http://domain.com
+./app-launching-service-broker -h=127.0.0.1 \
+                               -p=9999 \
+                               -u=user \
+                               -s=pswd \
+                               -d=true \
+                               -src=appname \
+                               -url=http://domain.com
 
