ab12ef71f0e948c966b1fa255193f9fd26fe656c
Dr Nic Williams <drnicwilliams@gmail.com>
2014-11-13 10:31:36 -0800

    fix env setup

--- bin/env.sh
+++ bin/env.sh
@@ -9,7 +9,7 @@ export ROOT_DOMAIN=${ROOT_DOMAIN:-gotapaas.com}
 export REDIRECT_URL=${REDIRECT_URL:-"http://localhost:9999/oauth2callback"}
 export AUTH_URL=${AUTH_URL:-"https://login.$ROOT_DOMAIN/oauth/authorize"}
 export TOKEN_URL=${TOKEN_URL:-"https://uaa.$ROOT_DOMAIN/oauth/token"}
-export API_URL=${TOKEN_URL:-"https://api.$ROOT_DOMAIN"}
+export API_URL=${API_URL:-"https://api.$ROOT_DOMAIN"}
 
 
 if [[ "${CF_PASS}X" == "X" ]]; then
