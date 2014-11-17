d345d6b295846f00cac59409230d49128e180f92
Long Nguyen <long.nguyen11288@gmail.com>
2014-11-17 09:47:47 -0500

    Updated to auto refresh levels

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -20,12 +20,40 @@ app.controller('AppNewCtrl', function($scope, $http) {
 app.controller('AppViewCtrl', function($scope, $http) {
 })
 
-app.controller('AppProvisionCtrl', function($scope, $http, $stateParams) {
+app.controller('AppProvisionCtrl', function($scope, $http, $stateParams, $timeout) {
   id = $stateParams.id
-  $http.get('/ui/apps/' + id ).success(function(data) {
-    console.log(data)
-    $scope.app = data
-  })
+  var levels = {
+    creating: 1,
+    uploading: 2,
+    provision_cloudera: 3,
+    provision_db: 4,
+    bind_services: 5,
+    restarting_atk: 6,
+    create_user: 7
+  }
+
+  $scope.findLevel = function(level) {
+    if (level == $scope.level) {
+      return "list-group-item-info"
+    } else if (level < $scope.level) {
+      return "list-group-item-success"
+    } else {
+      return ""
+    }
+  }
+
+  //5 second timer to refresh app and set level
+  var getApp = function() {
+    $http.get('/ui/apps/' + id ).success(function(data) {
+      $scope.app = data
+      $scope.level = levels[$scope.app.environment_json["APP_LAUNCHER_STATE"]]
+    })
+    $timeout(getApp, 5000);
+  }
+
+  var promise = $timeout(getApp, 5000);
+  getApp();
+
 })
 
 app.controller('NavBarCtrl', function($scope, $http) {
