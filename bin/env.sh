e535288bd6baaae9d01b03d4a3220cd70741bede
Long Nguyen <long.nguyen11288@gmail.com>
2014-12-18 16:06:53 -0500

    add ui to env

--- bin/env.sh
+++ bin/env.sh
@@ -3,8 +3,10 @@
 export CF_API=${CF_API:-"https://api.gotapaas.com"}
 export CF_SRC=${CF_SRC:-"$PWD/apps/cf-env"}
 export CF_DEP=${CF_DEP:-"consul|free,nats|free"}
+export CF_SETUP_PATH=${CF_SETUP_PATH:-"$PWD/apps/cf-env/setup.sh"}
 export CF_CATALOG_PATH=${CF_CATALOG_PATH:-"$PWD/catalog.json"}
 export CF_USER=${CF_USER:-"admin"}
+export UI=${UI:-"true"}
 export ROOT_DOMAIN=${ROOT_DOMAIN:-gotapaas.com}
 export REDIRECT_URL=${REDIRECT_URL:-"http://localhost:9999/oauth2callback"}
 export AUTH_URL=${AUTH_URL:-"https://login.$ROOT_DOMAIN/oauth/authorize"}
