49d21cc38d683c2192454872397a646367bc8d60
Grzegorz Swirski <grzegorz.swirski@intel.com>
2016-02-22 12:42:06 +0100

    Adding Godeps/_workspace to GOPATH in generate_swagger.sh script

--- generate_swagger.sh
+++ generate_swagger.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export GOPATH=$GOPATH:$(pwd)/Godeps/_workspace/
 go get -u github.com/go-swagger/go-swagger/cmd/swagger
 mkdir ./public
 swagger generate spec -o ./public/swagger.json
